c79c0517d6572417dec1dae4ac02e61c3057aaf4
feelzh <filzahshabrina27@gmail.com>
2024-04-18 10:32:24 +0700

    Menambahkan database

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Filzah Shabrina Saya tinggal di Cilincing Olahraga yang saya sukai adalah Bulutangkis";
+    }
+   
+}
